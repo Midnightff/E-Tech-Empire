b62972405c332da97b9ec67e2097ac9936f4ca4e
Jacqueline Bonilla Landaverde bljacqueline <bl0619032024@unad.edu.sv>
2024-10-25 22:07:06 -0600

    Agregando proveedores al modelo

--- E_Tech_Empire/tienda_e_tech_empire/models.py
+++ E_Tech_Empire/tienda_e_tech_empire/models.py
@@ -14,3 +14,12 @@ class Marca(models.Model):
 
     def __str__(self):
         return self.nombre
+    
+class Proveedor(models.Model):
+    nombre = models.CharField(max_length=255)
+    direccion = models.CharField(max_length=255)
+    telefono = models.CharField(max_length=20)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.nombre
