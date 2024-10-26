fbba02405488f76a4cb4d30c5c325c48b8992b8b
zury <cr0602032024@unab.edu.sv>
2024-10-26 15:29:03 -0600

    Arreglando errores de producto

--- E_Tech_Empire/producto/models.py
+++ E_Tech_Empire/producto/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+from categoria.models import Categoria
+from marca.models import Marca
+from proveedor.models import Proveedor
+
 # Create your models here.
 
 class Producto(models.Model):
@@ -10,6 +14,7 @@ class Producto(models.Model):
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
     marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
     proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE)
+
 
     def __str__(self):
         return self.nombre
