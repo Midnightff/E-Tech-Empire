5da26b29a38659c3bec41a4f97bde8216573a099
Midnight <alanalvarez19r@gmail.com>
2024-11-01 23:46:49 -0600

    crud categorias

--- E_Tech_Empire/categoria/urls.py
+++ E_Tech_Empire/categoria/urls.py
@@ -1,2 +1,10 @@
+
 from django.urls import path
 from categoria import views
+
+urlpatterns = [
+    path('categorias/', views.categoria_list, name='categoria_list'),
+    path('categoria/nueva/', views.categoria_create, name='categoria_create'),
+    path('categoria/editar/<int:pk>/', views.categoria_update, name='categoria_update'),
+    path('categoria/eliminar/<int:pk>/', views.categoria_delete, name='categoria_delete'),
+]
