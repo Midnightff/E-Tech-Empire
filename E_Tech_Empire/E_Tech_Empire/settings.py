9fcfbf474c52e626f94a044661e93cd7cfbbe638
Midnight <alanalvarez19r@gmail.com>
2024-11-01 22:47:08 -0600

    Se agrego panel admin

--- E_Tech_Empire/E_Tech_Empire/settings.py
+++ E_Tech_Empire/E_Tech_Empire/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 from decouple import config
 
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
     'pedido',
     'detalle_pedido',
     'envio',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -132,6 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
