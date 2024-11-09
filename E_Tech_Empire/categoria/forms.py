baebd9eaedc0d18a9a34726fb9faf4ad3ec54aea
Midnight <alanalvarez19r@gmail.com>
2024-11-09 14:48:14 -0600

    Se refactorizo categorias

--- E_Tech_Empire/categoria/forms.py
+++ E_Tech_Empire/categoria/forms.py
@@ -6,3 +6,12 @@ class CategoriaForm(forms.ModelForm):
     class Meta:
         model = Categoria
         fields = ['nombre', 'descripcion']
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nombre de la categoría'}),
+            'descripcion': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Descripción'}),
+        }
+    def clean_nombre(self):
+        nombre = self.cleaned_data.get('nombre')
+        if Categoria.objects.filter(nombre=nombre).exists():
+            raise forms.ValidationError("Ya existe una categoría con este nombre.")
+        return nombre
--- E_Tech_Empire/categoria/views.py
+++ E_Tech_Empire/categoria/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Categoria
 from .forms import CategoriaForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -8,15 +9,20 @@ def categoria_list(request):
     categorias = Categoria.objects.all()
     return render(request, 'categoria_list.html', {'categorias': categorias})
 
+
 def categoria_create(request):
     if request.method == 'POST':
         form = CategoriaForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Categoría guardada correctamente.')
             return redirect('categoria_list')
+        else:
+            messages.error(request, 'Error al guardar la categoría.')
     else:
         form = CategoriaForm()
-    return render(request, 'categoria_form.html', {'form': form})
+    return render(request, 'categoria_form.html', {'form': form, 'title': 'Crear Categoría'})
+
 
 def categoria_update(request, pk):
     categoria = get_object_or_404(Categoria, pk=pk)
@@ -24,13 +30,18 @@ def categoria_update(request, pk):
         form = CategoriaForm(request.POST, instance=categoria)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Categoría editada correctamente.')
             return redirect('categoria_list')
+        else:
+            messages.error(request, 'Error al editar la categoría.')
     else:
         form = CategoriaForm(instance=categoria)
-    return render(request, 'categoria_form.html', {'form': form})
+    return render(request, 'categoria_form.html', {'form': form, 'title': 'Editar Categoría'})
 
 def categoria_delete(request, pk):
     categoria = get_object_or_404(Categoria, pk=pk)
     if request.method == 'POST':
         categoria.delete()
+        messages.success(request, 'Categoría eliminada correctamente.')
         return redirect('categoria_list')
+    messages.error(request, 'Error al eliminar la categoría.')
