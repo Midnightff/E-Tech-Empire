1b2bf412465a72d390e4e79b3a24adc2e4722899
Midnight <alanalvarez19r@gmail.com>
2024-11-10 22:13:49 -0600

    se cambio la fk al nuevo modelo de user

--- E_Tech_Empire/pedido/models.py
+++ E_Tech_Empire/pedido/models.py
@@ -1,23 +1,19 @@
 from django.db import models
 
 from metodo_pago.models import MetodoPago
-from cliente.models import Cliente
+from user.models import CustomUser
+from estado.models import Estado
 
 # Create your models here.
 class Pedido(models.Model):
-    ESTADOS = [
-        ('pendiente', 'Pendiente'),
-        ('pagado', 'Pagado'),
-        ('cancelado', 'Cancelado'),
-        ('enviado', 'Enviado'),
-        ('entregado', 'Entregado'),
-    ]
-    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    codigo = models.CharField(max_length=255, unique=True, null=True, blank=True)
     fecha = models.DateTimeField()
     total = models.DecimalField(max_digits=10, decimal_places=2)
-    estado = models.CharField(max_length=10, choices=ESTADOS)
+    cliente = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    estado = models.ForeignKey(Estado, on_delete=models.CASCADE)
     metodo_pago = models.ForeignKey(MetodoPago, on_delete=models.CASCADE)
     fecha_pago = models.DateTimeField(null=True, blank=True)
+
 
     def _str_(self):
         return f'Pedido {self.id} - Cliente: {self.cliente.nombre}'
